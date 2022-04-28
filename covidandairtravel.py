fe7a6dcd2106f6b2bacfd498075249e86af4797d
Ronny Toribio <rt48634@huskies.bloomu.edu>
2022-04-28 10:28:41 -0400

    updated program and data

--- covidandairtravel.py
+++ covidandairtravel.py
@@ -2,7 +2,6 @@
 authors: Kadir O. Altunel, Ronny Toribio
 project: Covid and Air Travel
 """
-
 import csv
 import math
 
@@ -79,107 +78,54 @@ def r_squared(alpha, beta, x, y):
 
 
 def load_data(filename = "data/covidandairtravel.csv"):
-    fobj = open(filename)
-    file = csv.DictReader(fobj)
     year = []
     fuel = []
     enplanements = []
     revenue = []
-    for row in file:
-        year.append(int(row['Years']))
-        fuel.append(float(row['Fuel Consumption']))
-        enplanements.append(float(row['Enplanements']))
-        revenue.append(float(row['Revenue Streams']))
+    with open(filename, "r") as f:
+        file = csv.DictReader(f)
+        for row in file:
+            year.append(int(row['Years']))
+            fuel.append(float(row['Fuel Consumption']))
+            enplanements.append(float(row['Enplanements']))
+            revenue.append(float(row['Revenue Streams']))
     return year, fuel, enplanements, revenue
     
 
-def plot_data(year, fuel, enplanements, revenue):
+def plot_data(year, ydata, yname, color):
     plt.figure(figsize=(10, 10))
-    plt.plot(year, fuel, color='red', marker='o')
-    plt.title('Year vs Fuel Consumption', fontsize=14)
-    plt.xticks([2004, 2006, 2008 ,2010, 2012, 2014, 2016, 2018, 2020])
+    plt.plot(year, ydata, color=color, marker='o')
+    plt.title('Year vs {}'.format(yname), fontsize=14)
+    plt.xticks(year)
     plt.xlabel('Year', fontsize=14)
-    plt.ylabel('Fuel Consumption', fontsize=14)
-    plt.grid(True)
-    plt.show()
-
-    plt.figure(figsize=(10, 10))
-    plt.plot(year, enplanements, color='red', marker='o')
-    plt.title('Year vs Enplanements', fontsize=14)
-    plt.xticks([2004, 2006, 2008 ,2010, 2012, 2014, 2016, 2018, 2020])
-    plt.xlabel('Year', fontsize=14)
-    plt.ylabel('Enplanements', fontsize=14)
-    plt.grid(True)
-    plt.show()
-
-    plt.figure(figsize=(10, 10))
-    plt.plot(year, revenue, color='red', marker='o')
-    plt.title('Year vs Revenue Streams', fontsize=14)
-    plt.xticks([2004, 2006, 2008 ,2010, 2012, 2014, 2016, 2018, 2020])
-    plt.xlabel('Year', fontsize=14)
-    plt.ylabel('Revenue Streams', fontsize=14)
+    plt.ylabel(yname, fontsize=14)
     plt.grid(True)
     plt.show()
 
 
-def plot_regression_data(year, fuel, enplanements, revenue):
-    plot1 = sns.jointplot(x = year, y = fuel, data = row, kind = "reg");
-    plot1.ax_joint.set_xticks([2004, 2006, 2008 ,2010, 2012, 2014, 2016, 2018, 2020])
-    plot1.ax_joint.set_xlabel('Year', fontweight='bold')
-    plot1.ax_joint.set_ylabel('Fuel Consumption', fontweight='bold')
-    alpha, beta = least_squares_fit(year, fuel)
-    print("Pearson correlation coefficient for year vs fuel consumption is: ", 
-          r_squared(alpha, beta, year, fuel))
+def plot_regression_data(xdata, ydata, xname, yname, year):
+    plot = sns.jointplot(x = xdata, y = ydata, kind = "reg");
+    plot.ax_joint.set_xticks(year)
+    plot.ax_joint.set_xlabel(xname, fontweight='bold')
+    plot.ax_joint.set_ylabel(yname, fontweight='bold')
+    alpha, beta = least_squares_fit(xdata, ydata)
+    print("Pearson correlation coefficient for {} vs {} is: {}".format(xname, yname, r_squared(alpha, beta, xdata, ydata)))
 
-    plot2 = sns.jointplot(x = year, y = enplanements, data = row, kind = "reg");
-    plot2.ax_joint.set_xticks([2004, 2006, 2008 ,2010, 2012, 2014, 2016, 2018, 2020])
-    plot2.ax_joint.set_xlabel('Year', fontweight='bold')
-    plot2.ax_joint.set_ylabel('Enplanements', fontweight='bold')
-    alpha, beta = least_squares_fit(year, enplanements)
-    print("Pearson correlation coefficient for year vs enplanements is: ", 
-          r_squared(alpha, beta, year, enplanements))
-
-    plot3 = sns.jointplot(x = year, y = revenue, data = row, kind = "reg");
-    plot3.ax_joint.set_xticks([2004, 2006, 2008 ,2010, 2012, 2014, 2016, 2018, 2020])
-    plot3.ax_joint.set_xlabel('Year', fontweight='bold')
-    plot3.ax_joint.set_ylabel('Revenue Stream', fontweight='bold')
-    alpha, beta = least_squares_fit(year, revenue)
-    print("Pearson correlation coefficient for year vs revenue stream is: ", 
-          r_squared(alpha, beta, year, revenue))
-
-    plot4 = sns.jointplot(x = enplanements, y = fuel, data = row, kind = "reg");
-    plot4.ax_joint.set_xlabel('Enplanements', fontweight='bold')
-    plot4.ax_joint.set_ylabel('Fuel Consumption', fontweight='bold')
-    alpha, beta = least_squares_fit(enplanements, fuel)
-    print("Pearson correlation coefficient for enplanements vs fuel consumption is: ", 
-          r_squared(alpha, beta, enplanements, fuel))
-
-    plot5 = sns.jointplot(x = enplanements, y = revenue, data = row, kind = "reg");
-    plot5.ax_joint.set_xlabel('Enplanements', fontweight='bold')
-    plot5.ax_joint.set_ylabel('Revenue Stream', fontweight='bold')
-    alpha, beta = least_squares_fit(enplanements, revenue)
-    print("Pearson correlation coefficient for enplanements vs revenue stream is: ", 
-          r_squared(alpha, beta, enplanements, revenue))
-
-    plot6 = sns.jointplot(x = fuel, y = revenue, data = row, kind = "reg");
-    plot6.ax_joint.set_xlabel('Fuel Consumption', fontweight='bold')
-    plot6.ax_joint.set_ylabel('Revenue Stream', fontweight='bold')
-    alpha, beta = least_squares_fit(fuel, revenue)
-    print("Pearson correlation coefficient for fuel consumption vs revenue stream is: ", 
-          r_squared(alpha, beta, fuel, revenue))
-    print(year)
 
 if __name__ == "__main__":
     year, fuel, enplanements, revenue = load_data("data/covidandairtravel.csv")
-    plot_data(year, fuel, enplanements, revenue)
+    plot_data(year, fuel, "Fuel Consumption", "r")
+    plot_data(year, enplanements, "Enplanements", "g")
+    plot_data(year, revenue, "Revenue Streams", "b")
     print("The fit for year vs fuel consumption is: ",  least_squares_fit(year, fuel))
     print("The fit for year vs enplanements is: ",  least_squares_fit(year, enplanements))
     print("The fit for year vs revenue stream is: ",  least_squares_fit(year, revenue))
     print("The fit for enplanements vs fuel consumption is: ",  least_squares_fit(enplanements, fuel))
     print("The fit for enplanements vs revenue stream is: ",  least_squares_fit(enplanements, revenue))
     print("The fit for fuel consumption vs revenue stream is: ",  least_squares_fit(fuel, revenue))
-    plot_regression_data(year, fuel, enplanements, revenue)
-
-
-
-
+    plot_regression_data(year, fuel, "Year", "Fuel Consumption", year)
+    plot_regression_data(year, enplanements, "Year", "Enplanements", year)
+    plot_regression_data(year, revenue, "Year", "Revenue Streams", year)
+    plot_regression_data(enplanements, fuel, "Enplanements", "Fuel Consumption", year)
+    plot_regression_data(enplanements, revenue, "Enplanements", "Revenue Streams", year)
+    plot_regression_data(fuel, revenue, "Fuel Consumption", "Revenue Streams", year)
